4ff27e6340776baf259c40fbdacb014550f6bd2b
Your Name <przemyslaw.erbacher@gmail.com>
2020-03-02 15:59:11 +0100

    Add animations for move in and move out hidden blocks

--- js/site.js
+++ js/site.js
@@ -4,25 +4,36 @@ var mainOffsetTop = document.querySelector('.products').offsetTop;
 var menuButton = document.querySelector('.menu');
 var hiddenMenu = document.querySelector('.hidden-menu');
 var closeHiddenMenuButton = document.querySelector('.close-hidden-menu');
+var basketbutton = document.querySelector('.basket');
+var order = document.querySelector(".order");
+var closeOrderButton = document.querySelector('.close-order');
 
-menuButton.addEventListener('click', function () {
-	hiddenMenu.style.left = 0;
+//basket move in effect
+basketbutton.addEventListener('click', function () {
+	order.classList.add('active');
+	order.classList.remove('disabled');
 })
 
+//basket move out effect
+closeOrderButton.addEventListener('click', function () {
+	order.classList.remove('active');
+	order.classList.add('disabled');
+
+})
+
+//menu move in effect
+menuButton.addEventListener('click', function () {
+	hiddenMenu.classList.add('active');
+	hiddenMenu.classList.remove('disabled');
+})
+
+//menu move out effect
 closeHiddenMenuButton.addEventListener('click', function () {
-	hiddenMenu.style.left = "-100%";
+	hiddenMenu.classList.remove('active');
+	hiddenMenu.classList.add('disabled');
 })
 
 //rotate expand button
 expandButton.addEventListener('click', function () {
 	this.classList.toggle('rotate');
-})
-
-window.addEventListener('click', function () {
-	if (hiddenMenu.classList.contains('active')) {
-		hiddenMenu.style.left = "-100%";
-		hiddenMenu.classList.remove("active");
-		this.alert('click');
-
-	}
 })
